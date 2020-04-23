6b22c692c4a8db1539813fc098b09d199fb5004f
aomtem2 <53507652+aomtem2@users.noreply.github.com>
2020-04-23 23:31:58 +0700

    Add files via upload

---
+++ t.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Websever</title>
+   
+
+<body>
+    <br /><br />
+    <div class="container">
+        <h3 align="center">Button</h3>
+        
+    </div>
+</body>
+
+</html>
